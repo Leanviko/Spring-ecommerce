a97abdaefa5ed217f1b1511c0ca694765690df0a
LeanViko <leangant@gmail.com>
2023-03-27 21:05:36 -0300

    creacion de la estructura del proyecto y las clases model

---
+++ src/main/java/com/leansoft/ecommerce/model/Usuario.java
@@ -0,0 +1,21 @@
+package com.leansoft.ecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Usuario {
+    private Integer id;
+    private String nombre;
+    private String username;
+    private String email;
+    private String direccion;
+    private String telefono;
+    private String tipo;
+    private String password;
+}

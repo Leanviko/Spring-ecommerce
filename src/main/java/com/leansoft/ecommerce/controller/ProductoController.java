308deba9f7721d60076a9938cbb25517a6759809
LeanViko <leangant@gmail.com>
2023-03-31 09:15:57 -0300

    Implementacion para guardar la imagen del producto

--- src/main/java/com/leansoft/ecommerce/controller/ProductoController.java
+++ src/main/java/com/leansoft/ecommerce/controller/ProductoController.java
@@ -3,25 +3,29 @@ package com.leansoft.ecommerce.controller;
 import com.leansoft.ecommerce.model.Producto;
 import com.leansoft.ecommerce.model.Usuario;
 import com.leansoft.ecommerce.service.IProductoService;
+import com.leansoft.ecommerce.service.UploadFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController{
 
+    private final Logger LOGGER =  LoggerFactory.getLogger(ProductoController.class);
+
     @Autowired
     private IProductoService productoService;
-    private final Logger LOGGER =  LoggerFactory.getLogger(ProductoController.class);
+    @Autowired
+    private UploadFileService fileService;
+
     @GetMapping("")
     public String show(Model model){
         model.addAttribute("listaProductos",productoService.getAll());
@@ -34,10 +38,27 @@ public class ProductoController{
     }
 
     @PostMapping("/save")
-    public String save(Producto producto){
+    public String save(Producto producto, @RequestParam("img")MultipartFile file) throws IOException {
         LOGGER.info("Este es el objeto producto {}",producto);
         Usuario u = new Usuario(1,"","","","","","","",null,null);
         producto.setUsuario(u);
+
+        //imagen
+        if (producto.getId()==null){ // cuando se crea un producto nuevo
+            String enlaceProd = fileService.saveImage(file);
+            producto.setImagen(enlaceProd);
+        }else{ //Si editamos
+            if (file.isEmpty()){//editamos el producto pero no subimos imagen
+                Producto p = new Producto();
+                p = productoService.getById(producto.getId()).get();
+
+                producto.setImagen(p.getImagen());//Asignamos la imagen que ya tenia el Prod. si modificar
+            }else{
+                String enlaceProd = fileService.saveImage(file);
+                producto.setImagen(enlaceProd);
+            }
+        }
+
         productoService.save(producto);
         return "redirect:/productos";
     }
--- src/main/java/com/leansoft/ecommerce/service/UploadFileService.java
+++ src/main/java/com/leansoft/ecommerce/service/UploadFileService.java
@@ -1,7 +1,6 @@
 package com.leansoft.ecommerce.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;

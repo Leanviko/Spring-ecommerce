dee8c065216122b5a3490c062629d72972b82a4c
LeanViko <leangant@gmail.com>
2023-04-10 16:08:41 -0300

    Metodo para generar el numero de orden

--- src/main/java/com/leansoft/ecommerce/service/IOrdenService.java
+++ src/main/java/com/leansoft/ecommerce/service/IOrdenService.java
@@ -4,6 +4,9 @@ import com.leansoft.ecommerce.model.Orden;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface IOrdenService {
     Orden save(Orden orden);
+    List<Orden> findAll();
 }
--- src/main/java/com/leansoft/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/leansoft/ecommerce/service/OrdenServiceImpl.java
@@ -5,6 +5,9 @@ import com.leansoft.ecommerce.repository.IOrdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrdenServiceImpl implements IOrdenService{
 
@@ -14,5 +17,34 @@ public class OrdenServiceImpl implements IOrdenService{
     @Override
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
+    }
+
+    @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
+
+    public String generarNumeroOrden(){
+        int numero=0;
+        String numeroConcatenado = "";
+        List<Orden> ordenes = this.findAll();
+        List<Integer> numeros = new ArrayList<>();
+
+        ordenes.stream().forEach(o->numeros.add(Integer.parseInt(o.getNumero())));
+
+        if (ordenes.isEmpty()){
+            numero=1;
+        }else{
+            numero= numeros.stream().max(Integer::compare).get();//compara numeros a lo largo de la lista y devuelve el maximo
+            numero++;
+        }
+        String numeroString = String.valueOf(numero);
+        StringBuilder ceros = new StringBuilder();
+        for (int i=0;i<=9-numeroString.length();i++){
+            ceros.append("0");
+        }
+        numeroConcatenado = ceros+numeroString;
+
+        return numeroConcatenado;
     }
 }

ba958af1994f910161dd77cbf3019c29f2cc8565
LeanViko <leangant@gmail.com>
2023-04-12 16:52:53 -0300

    Funcionalidad cerrar sesion de usuario

--- src/main/java/com/leansoft/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/leansoft/ecommerce/controller/UsuarioController.java
@@ -86,4 +86,10 @@ public class UsuarioController {
         model.addAttribute("sesion",session.getAttribute("idusuario"));
      return "usuario/detallecompra";
     }
+
+    @GetMapping("/cerrar")
+    public String cerrarSesion(HttpSession session){
+        session.removeAttribute("idusuario");
+        return "redirect:/";
+    }
     }
